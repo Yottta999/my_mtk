79c2b78cf91e4e16408a9e8cacc657456d0fd80c
Yottta999 <yota.a0929@icloud.com>
2025-12-24 14:36:51 +0900

    Update usr.c

--- usr.c
+++ usr.c
@@ -1,402 +1,281 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "mtk_c.h"
 
-#define FIELD_WIDTH 30
-#define FIELD_HEIGHT 40
-#define CONTROLLER_SIZE 5
+/* --- ハードウェア定義 (環境に合わせて調整) --- */
+#define REGBASE 0xfff000
+volatile unsigned short *USTCNT1 = (unsigned short *)(REGBASE + 0x900);
+volatile unsigned short *URX1    = (unsigned short *)(REGBASE + 0x904);
+volatile unsigned short *USTCNT2 = (unsigned short *)(REGBASE + 0x940); // ※要確認
+volatile unsigned short *URX2    = (unsigned short *)(REGBASE + 0x944); // ※要確認
 
-/* フィールド管理 */
+/* --- ゲーム設定 --- */
+#define WIDTH  20
+#define HEIGHT 15
+#define PADDLE_W 3
+#define GOAL_SCORE 3
+
+/* キー定義 */
+#define KEY_SWITCH ' '  // 方向転換
+#define KEY_RESET  'r'  // 強制リセット
+
+/* ANSI エスケープシーケンス */
+#define ESC "\x1b"
+#define CLS "\x1b[2J"
+#define HIDE_CURSOR "\x1b[?25l"
+#define COLOR_RESET "\x1b[0m"
+#define COLOR_BALL  "\x1b[33m" // 黄
+#define COLOR_P1    "\x1b[36m" // シアン
+#define COLOR_P2    "\x1b[35m" // マゼンタ
+#define COLOR_WALL  "\x1b[37m" // 白
+
+/* グローバルゲーム状態 */
 typedef struct {
-    char field[FIELD_HEIGHT][FIELD_WIDTH];
-    int p1_x;
-    int p1_y;
-    int p2_x;
-    int p2_y;
-    int puck_x;
-    int puck_y;
-    int puck_vx;
-    int puck_vy;
-    int my_score;
-    int opp_score;
-    int is_player1;
-    int update_display;
-    int game_running;
-    unsigned int rand_seed;
-} GameState;
+    int x, y;           // ボール位置
+    int vx, vy;         // ボール速度
+    int old_x, old_y;   // 前回位置（消去用）
+} Ball;
 
-GameState game;
+typedef struct {
+    int x;              // パドル左端位置 (yは固定)
+    int dir;            // 移動方向 (1:右, -1:左)
+    int old_x;          // 前回位置
+    int score;
+} Paddle;
 
-/* 簡易乱数生成器 */
-int my_rand() {
-    game.rand_seed = game.rand_seed * 1103515245 + 12345;
-    return (game.rand_seed / 65536) % 32768;
-}
+/* 共有データ */
+Ball ball;
+Paddle p1, p2; // P1は y=HEIGHT-2, P2は y=1
+int game_state; // 0:待機, 1:プレイ中, 2:終了
+int hit_count;  // 反射回数（速度調整用）
+int winner;
 
-void init_game(int is_p1) {
-    game.is_player1 = is_p1;
-    game.p1_x = FIELD_WIDTH / 2;
-    game.p1_y = 3;
-    game.p2_x = FIELD_WIDTH / 2;
-    game.p2_y = FIELD_HEIGHT - 4;
-    game.puck_x = FIELD_WIDTH / 2;
-    game.puck_y = FIELD_HEIGHT / 2;
-    game.my_score = 0;
-    game.opp_score = 0;
-    game.update_display = 1;
-    game.game_running = 1;
-    game.rand_seed = 12345;
-    
-    if (is_p1) {
-        game.puck_vx = 0;
-        game.puck_vy = 1;
+/* --- 関数: 入出力 --- */
+
+/* 座標移動 (P2は180度回転して出力) */
+void term_goto(FILE *fp, int id, int x, int y) {
+    if (id == 0) {
+        // P1: 通常視点
+        fprintf(fp, "\x1b[%d;%dH", y + 1, x + 1);
     } else {
-        game.puck_vx = 0;
-        game.puck_vy = 0;
+        // P2: 反転視点 (xもyも逆)
+        fprintf(fp, "\x1b[%d;%dH", (HEIGHT - 1 - y) + 1, (WIDTH - 1 - x) + 1);
     }
 }
 
-/* フィールドを初期化 */
-void clear_field() {
-    for (int y = 0; y < FIELD_HEIGHT; y++) {
-        for (int x = 0; x < FIELD_WIDTH; x++) {
-            if (x == 0 || x == FIELD_WIDTH - 1) {
-                game.field[y][x] = '|';
-            } else if (y == 0 || y == FIELD_HEIGHT - 1) {
-                game.field[y][x] = '-';
-            } else if (y == FIELD_HEIGHT / 2) {
-                game.field[y][x] = ':';
-            } else {
-                game.field[y][x] = ' ';
+/* 文字描画 */
+void term_put(FILE *fp, int id, int x, int y, char *c) {
+    term_goto(fp, id, x, y);
+    fprintf(fp, "%s", c);
+} 
+
+/* --- ゲームロジック --- */
+
+void init_round() {
+    ball.x = WIDTH / 2;
+    ball.y = HEIGHT / 2;
+    ball.vx = (rand() % 2 == 0) ? 1 : -1;
+    ball.vy = (hit_count % 2 == 0) ? 1 : -1;
+    ball.old_x = ball.x;
+    ball.old_y = ball.y;
+
+    p1.x = WIDTH / 2 - 1; p1.dir = 1; p1.old_x = p1.x;
+    p2.x = WIDTH / 2 - 1; p2.dir = 1; p2.old_x = p2.x;
+    
+    hit_count = 0;
+}
+
+void draw_frame_static(FILE *fp, int id) {
+    fprintf(fp, CLS);
+    fprintf(fp, HIDE_CURSOR);
+    
+    // 枠描画
+    term_put(fp, id, 0, 0, COLOR_WALL, "+");
+    for(int i=1; i<WIDTH-1; i++) fprintf(fp, "-");
+    fprintf(fp, "+");
+
+    // スコア表示
+    term_goto(fp, id, 2, HEIGHT+1); // 画面外下部
+    fprintf(fp, COLOR_RESET "1P: %d  2P: %d", p1.score, p2.score);
+}
+
+/* 差分描画システム*/
+void update_view(int id) {
+    FILE *fp = (id == 0) ? com0out : com1out;
+    
+    // 1. ボールの消去と描画
+    if (ball.x != ball.old_x || ball.y != ball.old_y) {
+        term_put(fp, id, ball.old_x, ball.old_y, COLOR_RESET, " "); // 消す
+        term_put(fp, id, ball.x, ball.y, COLOR_BALL, "O");          // 描く
+    }
+
+    // 2. パドルの消去と描画 (P1)
+    if (p1.x != p1.old_x) {
+        // P1は論理座標 y = HEIGHT - 2
+        for(int i=0; i<PADDLE_W; i++) 
+            term_put(fp, id, p1.old_x + i, HEIGHT - 2, COLOR_RESET, " ");
+        for(int i=0; i<PADDLE_W; i++) 
+            term_put(fp, id, p1.x + i, HEIGHT - 2, (id==0)?COLOR_P1:COLOR_P2, "=");
+    }
+
+    // 3. パドルの消去と描画 (P2)
+    if (p2.x != p2.old_x) {
+        // P2は論理座標 y = 1
+        for(int i=0; i<PADDLE_W; i++) 
+            term_put(fp, id, p2.old_x + i, 1, COLOR_RESET, " ");
+        for(int i=0; i<PADDLE_W; i++) 
+            term_put(fp, id, p2.x + i, 1, (id==1)?COLOR_P1:COLOR_P2, "=");
+    }
+}
+
+void move_paddle(Paddle *p) {
+    p->old_x = p->x;
+    p->x += p->dir;
+    
+    // 壁反射
+    if (p->x < 1) {
+        p->x = 1;
+        p->dir = 1; // 強制反転
+    }
+    if (p->x > WIDTH - 1 - PADDLE_W) {
+        p->x = WIDTH - 1 - PADDLE_W;
+        p->dir = -1;
+    }
+}
+
+void update_physics() {
+    ball.old_x = ball.x;
+    ball.old_y = ball.y;
+    
+    ball.x += ball.vx;
+    ball.y += ball.vy;
+
+    // 壁反射 (左右)
+    if (ball.x <= 0 || ball.x >= WIDTH - 1) {
+        ball.vx = -ball.vx;
+        ball.x += ball.vx; // めり込み防止
+    }
+
+    // P2パドル判定 (y=1付近)
+    if (ball.y == 1) {
+        if (ball.x >= p2.x && ball.x < p2.x + PADDLE_W) {
+            ball.vy = 1; // 下へ弾く
+            hit_count++;
+            // ランダム反射
+            ball.vx = (rand() % 3) - 1; // -1, 0, 1
+            if (ball.vx == 0) ball.vx = (rand()%2)?1:-1; // 0回避
+        }
+    }
+    
+    // P1パドル判定 (y=HEIGHT-2付近)
+    if (ball.y == HEIGHT - 2) {
+        if (ball.x >= p1.x && ball.x < p1.x + PADDLE_W) {
+            ball.vy = -1; // 上へ弾く
+            hit_count++;
+            ball.vx = (rand() % 3) - 1;
+            if (ball.vx == 0) ball.vx = (rand()%2)?1:-1;
+        }
+    }
+
+    // 得点判定
+    if (ball.y <= 0) {
+        p1.score++; // P2が落とした
+        init_round();
+        draw_frame_static(com0out, 0); // 画面リフレッシュ
+        draw_frame_static(com1out, 1);
+    }
+    else if (ball.y >= HEIGHT - 1) {
+        p2.score++; // P1が落とした
+        init_round();
+        draw_frame_static(com0out, 0);
+        draw_frame_static(com1out, 1);
+    }
+}
+
+/* メインタスク (ゲーム管理) */
+void task1() {
+    // コンソール初期化
+    game_state = 0;
+    p1.score = 0; p2.score = 0;
+    
+    fprintf(com0out, CLS "PRESS SPACE TO START\n");
+    fprintf(com1out, CLS "PRESS SPACE TO START\n");
+
+    while(1) {
+        // --- 入力処理 (ノンブロッキング) ---
+        char c1 = inkey(0);
+        char c2 = inkey(1);
+
+        if (game_state == 0) {
+            // 待機画面
+            if (c1 == KEY_SWITCH || c2 == KEY_SWITCH) {
+                game_state = 1;
+                init_round();
+                draw_frame_static(com0out, 0);
+                draw_frame_static(com1out, 1);
             }
+            continue;
         }
-    }
-}
-
-/* フィールドに要素を配置 */
-void update_field() {
-    clear_field();
-    
-    /* プレイヤー1のコントローラを配置 */
-    for (int dx = -CONTROLLER_SIZE/2; dx <= CONTROLLER_SIZE/2; dx++) {
-        int x = game.p1_x + dx;
-        if (x > 0 && x < FIELD_WIDTH - 1) {
-            if (game.p1_y > 0 && game.p1_y < FIELD_HEIGHT - 1) {
-                game.field[game.p1_y][x] = '1';
+        else if (game_state == 2) {
+            // 勝利画面
+            if (c1 == KEY_SWITCH || c2 == KEY_SWITCH) {
+                p1.score = 0; p2.score = 0;
+                game_state = 0;
+                fprintf(com0out, CLS "RESTART? PRESS SPACE\n");
+                fprintf(com1out, CLS "RESTART? PRESS SPACE\n");
             }
+            continue;
         }
-    }
-    
-    /* プレイヤー2のコントローラを配置 */
-    for (int dx = -CONTROLLER_SIZE/2; dx <= CONTROLLER_SIZE/2; dx++) {
-        int x = game.p2_x + dx;
-        if (x > 0 && x < FIELD_WIDTH - 1) {
-            if (game.p2_y > 0 && game.p2_y < FIELD_HEIGHT - 1) {
-                game.field[game.p2_y][x] = '2';
-            }
-        }
-    }
-    
-    /* パックを配置 */
-    if (game.puck_x > 0 && game.puck_x < FIELD_WIDTH - 1 &&
-        game.puck_y > 0 && game.puck_y < FIELD_HEIGHT - 1) {
-        game.field[game.puck_y][game.puck_x] = 'O';
-    }
-}
-
-/* 画面描画 */
-void draw_game() {
-    printf("\x1b[2J\x1b[H");
-    
-    if (game.is_player1) {
-        printf("=== PLAYER 1 (TOP) ===\n");
-        printf("YOU: %d | OPP: %d\n", game.my_score, game.opp_score);
-    } else {
-        printf("=== PLAYER 2 (BOTTOM) ===\n");
-        printf("OPP: %d | YOU: %d\n", game.opp_score, game.my_score);
-    }
-    
-    for (int y = 0; y < FIELD_HEIGHT; y++) {
-        for (int x = 0; x < FIELD_WIDTH; x++) {
-            printf("%c", game.field[y][x]);
-        }
-        printf("\n");
-    }
-    
-    if (game.is_player1) {
-        printf("Controls: a=LEFT d=RIGHT q=QUIT\n");
-    } else {
-        printf("Controls: a=LEFT d=RIGHT q=QUIT\n");
-    }
-}
-
-/* パック物理演算 */
-void update_puck() {
-    game.puck_x += game.puck_vx;
-    game.puck_y += game.puck_vy;
-    
-    /* 左右の壁で反射 */
-    if (game.puck_x <= 1) {
-        game.puck_x = 1;
-        game.puck_vx = -game.puck_vx;
-    }
-    if (game.puck_x >= FIELD_WIDTH - 2) {
-        game.puck_x = FIELD_WIDTH - 2;
-        game.puck_vx = -game.puck_vx;
-    }
-    
-    /* プレイヤー1のコントローラとの衝突 */
-    if (game.puck_y == game.p1_y || game.puck_y == game.p1_y + 1) {
-        int dx = game.puck_x - game.p1_x;
-        if (dx >= -CONTROLLER_SIZE/2 && dx <= CONTROLLER_SIZE/2 && game.puck_vy < 0) {
-            game.puck_vy = -game.puck_vy;
-            /* 乱数で反射方向を決定 */
-            int r = my_rand() % 5;
-            if (r == 0) game.puck_vx = -1;
-            else if (r == 1) game.puck_vx = 1;
-            else if (r == 2) game.puck_vx = -2;
-            else if (r == 3) game.puck_vx = 2;
-            else game.puck_vx = 0;
-        }
-    }
-    
-    /* プレイヤー2のコントローラとの衝突 */
-    if (game.puck_y == game.p2_y || game.puck_y == game.p2_y - 1) {
-        int dx = game.puck_x - game.p2_x;
-        if (dx >= -CONTROLLER_SIZE/2 && dx <= CONTROLLER_SIZE/2 && game.puck_vy > 0) {
-            game.puck_vy = -game.puck_vy;
-            /* 乱数で反射方向を決定 */
-            int r = my_rand() % 5;
-            if (r == 0) game.puck_vx = -1;
-            else if (r == 1) game.puck_vx = 1;
-            else if (r == 2) game.puck_vx = -2;
-            else if (r == 3) game.puck_vx = 2;
-            else game.puck_vx = 0;
-        }
-    }
-    
-    /* 得点判定 */
-    if (game.puck_y <= 0) {
-        if (game.is_player1) {
-            game.opp_score++;
-        } else {
-            game.my_score++;
-        }
-        game.puck_x = FIELD_WIDTH / 2;
-        game.puck_y = FIELD_HEIGHT / 2;
-        game.puck_vx = 0;
-        game.puck_vy = 1;
-        game.update_display = 1;
-    }
-    
-    if (game.puck_y >= FIELD_HEIGHT - 1) {
-        if (game.is_player1) {
-            game.my_score++;
-        } else {
-            game.opp_score++;
-        }
-        game.puck_x = FIELD_WIDTH / 2;
-        game.puck_y = FIELD_HEIGHT / 2;
-        game.puck_vx = 0;
-        game.puck_vy = -1;
-        game.update_display = 1;
-    }
-}
-
-/* タスク1: キーボード入力処理 */
-void keyboard_input_task() {
-    char c[2];
-    while (1) {
-        fscanf(stdin, "%1s", c);
-        
-        P(1);
-        if (game.is_player1) {
-            if (c[0] == 'a' && game.p1_x > CONTROLLER_SIZE/2 + 1) {
-                game.p1_x--;
-                game.update_display = 1;
-            } else if (c[0] == 'd' && game.p1_x < FIELD_WIDTH - CONTROLLER_SIZE/2 - 2) {
-                game.p1_x++;
-                game.update_display = 1;
-            }
-        } else {
-            if (c[0] == 'a' && game.p2_x > CONTROLLER_SIZE/2 + 1) {
-                game.p2_x--;
-                game.update_display = 1;
-            } else if (c[0] == 'd' && game.p2_x < FIELD_WIDTH - CONTROLLER_SIZE/2 - 2) {
-                game.p2_x++;
-                game.update_display = 1;
-            }
-        }
-        
-        if (c[0] == 'q') {
-            game.game_running = 0;
-        }
-        V(1);
-    }
-}
-
-/* タスク2: データ送信処理 */
-void send_task() {
-    while (1) {
-        for (int i = 0; i < 100000; i++);
-        
-        P(1);
-        if (game.is_player1) {
-            fprintf(com0out, "P1%02d%02d%02d%02d%02d%02d%d%d\n",
-                    game.p1_x, game.p1_y,
-                    game.puck_x, game.puck_y,
-                    game.puck_vx + 10, game.puck_vy + 10,
-                    game.my_score, game.opp_score);
-        } else {
-            fprintf(com0out, "P2%02d%02d%02d%02d%02d%02d%d%d\n",
-                    game.p2_x, game.p2_y,
-                    game.puck_x, game.puck_y,
-                    game.puck_vx + 10, game.puck_vy + 10,
-                    game.my_score, game.opp_score);
-        }
-        V(1);
-    }
-}
-
-/* タスク3: データ受信処理 */
-void receive_task() {
-    char buf[32];
-    int player_id, cx, cy, px, py, pvx, pvy, s1, s2;
-    
-    while (1) {
-        if (fscanf(com0in, "%s", buf) == 1) {
-            if (buf[0] == 'P' && (buf[1] == '1' || buf[1] == '2')) {
-                player_id = buf[1] - '0';
-                
-                if ((game.is_player1 && player_id == 2) || 
-                    (!game.is_player1 && player_id == 1)) {
-                    
-                    cx = (buf[2] - '0') * 10 + (buf[3] - '0');
-                    cy = (buf[4] - '0') * 10 + (buf[5] - '0');
-                    px = (buf[6] - '0') * 10 + (buf[7] - '0');
-                    py = (buf[8] - '0') * 10 + (buf[9] - '0');
-                    pvx = (buf[10] - '0') * 10 + (buf[11] - '0') - 10;
-                    pvy = (buf[12] - '0') * 10 + (buf[13] - '0') - 10;
-                    s1 = buf[14] - '0';
-                    s2 = buf[15] - '0';
-                    
-                    P(1);
-                    if (game.is_player1) {
-                        game.p2_x = cx;
-                        game.p2_y = cy;
-                    } else {
-                        game.p1_x = cx;
-                        game.p1_y = cy;
-                        game.puck_x = px;
-                        game.puck_y = py;
-                        game.puck_vx = pvx;
-                        game.puck_vy = pvy;
-                    }
-                    game.update_display = 1;
-                    V(1);
-                }
-            }
+        // 勝敗判定
+        if (p1.score >= GOAL_SCORE || p2.score >= GOAL_SCORE) {
+            game_state = 2;
+            winner = (p1.score >= GOAL_SCORE) ? 1 : 2;
+            
+            fprintf(com0out, CLS "\x1b[10;10H %s \n", (winner==1)?"YOU WIN!":"YOU LOSE");
+            fprintf(com1out, CLS "\x1b[10;10H %s \n", (winner==2)?"YOU WIN!":"YOU LOSE");
         }
     }
 }
 
-/* タスク4: パック物理演算処理（Player1のみ） */
-void physics_task() {
-    while (1) {
-        for (int i = 0; i < 80000; i++);
-        
-        P(1);
-        if (game.is_player1 && game.game_running) {
-            update_puck();
-            game.update_display = 1;
-        }
-        V(1);
+void task2() {
+    while(1){
+        char c1 = inkey(0);
+        if (c1 == KEY_SWITCH) p1.dir *= -1;
+        move_paddle(&p1);
     }
 }
 
-/* タスク5: 画面描画処理 */
-void display_task() {
-    while (1) {
-        for (int i = 0; i < 150000; i++);
-        
-        P(1);
-        if (game.update_display) {
-            update_field();
-            draw_game();
-            game.update_display = 0;
-        }
-        
-        if (!game.game_running) {
-            printf("\n=== GAME OVER ===\n");
-            printf("Final Score - ");
-            if (game.is_player1) {
-                printf("YOU: %d | OPP: %d\n", game.my_score, game.opp_score);
-            } else {
-                printf("OPP: %d | YOU: %d\n", game.opp_score, game.my_score);
-            }
-            V(1);
-            while(1);
-        }
-        V(1);
+void task3() {
+    while(1){
+        char c2 = inkey(1);
+        if (c2 == KEY_SWITCH) p2.dir *= -1;
+        move_paddle(&p2);
     }
 }
 
-/* タスク6: ゲーム状態監視 */
-void monitor_task() {
-    int prev_my_score = 0;
-    int prev_opp_score = 0;
-    
-    while (1) {
-        for (int i = 0; i < 200000; i++);
-        
-        P(1);
-        if (game.my_score != prev_my_score || game.opp_score != prev_opp_score) {
-            prev_my_score = game.my_score;
-            prev_opp_score = game.opp_score;
-            game.update_display = 1;
+void task4() {
+    while(1){
+        if(game_state == 1){
+            update_physics();
+            update_view(0);
+            update_view(1);
         }
-        V(1);
+        int wait = 150 - (hit_count * 5);
+        if (wait < 30) wait = 30; // 最速リミット
+        for (int i = 0; i < wait*10000; i++);
     }
 }
 
+
+/* main.c の代わり */
 int main() {
-    char choice[2];
-    
-    printf("=============================\n");
-    printf("   HOCKEY GAME - VERTICAL\n");
-    printf("=============================\n");
-    printf("Are you Player 1 or 2? (1/2): ");
-    
-    fd_mapping();
     init_kernel();
-    
-    scanf("%1s", choice);
-    
-    if (choice[0] == '1') {
-        init_game(1);
-        printf("You are Player 1 (TOP side)\n");
-        printf("Controller marked as '1'\n");
-    } else {
-        init_game(0);
-        printf("You are Player 2 (BOTTOM side)\n");
-        printf("Controller marked as '2'\n");
-    }
-    
-    printf("Starting game...\n");
-    
-    for (int i = 0; i < 2000000; i++);
-    
-    /* タスク登録 */
-    set_task(keyboard_input_task);  /* タスク1: キーボード入力 */
-    set_task(send_task);            /* タスク2: データ送信 */
-    set_task(receive_task);         /* タスク3: データ受信 */
-    set_task(physics_task);         /* タスク4: 物理演算 */
-    set_task(display_task);         /* タスク5: 画面描画 */
-    set_task(monitor_task);         /* タスク6: 状態監視 */
-    
-    update_field();
-    draw_game();
-    
+    fd_mapping();
+    set_task(task1);
+    set_task(task2);
+    set_task(task3);
+    set_task(task4);
+
     begin_sch();
-    
     return 0;
 }
